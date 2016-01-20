c74ea40dd58ed4a6eec2e8eb694810b756291b15
Sarn Wattanasri <sarn@Sarns-MacBook-Pro.local>
2016-01-20 12:37:00 -0800

    Add the SwitchCell delegate

--- Yelp/SwitchCell.swift
+++ Yelp/SwitchCell.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
+@objc protocol SwitchCellDelegate {
+    //events passing back: to propagate events
+    
+    //method name: object that fires event
+    //first arguement: the same object
+    //optional - if you not interested in the event, it's ok
+    optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+}
+
 class SwitchCell: UITableViewCell {
     
     @IBOutlet weak var switchLabel: UILabel!
     @IBOutlet weak var onSwitch: UISwitch!
+    
+    //object pass it back to when event fire
+    weak var delegate: SwitchCellDelegate?
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        onSwitch.addTarget(self, action: "switchValueChanged", forControlEvents: UIControlEvents.ValueChanged)
+    }
+    
+    func switchValueChanged() {
+        delegate?.switchCell?(self, didChangeValue: onSwitch.on)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
