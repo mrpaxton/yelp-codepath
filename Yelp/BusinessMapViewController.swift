eb781cc638fee7b85ca735d1c82d7e71b366f015
Sarn Wattanasri <sarn@Sarns-MacBook-Pro.local>
2016-01-29 02:46:28 -0800

    Integrate the map and complete about 50% codes for map

--- Yelp/BusinessMapViewController.swift
+++ Yelp/BusinessMapViewController.swift
@@ -7,16 +7,53 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class BusinessMapViewController: UIViewController {
+class BusinessMapViewController: UIViewController, MKMapViewDelegate {
 
-    @IBOutlet weak var mapView: UIView!
     
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "customAnnotationView"
+        
+        // custom image annotation
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+        if (annotationView == nil) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        }
+        else {
+            annotationView!.annotation = annotation
+        }
+        annotationView!.image = UIImage(named: "FilterIcon")
+        
+        return annotationView
+    }
+    
+    func addAnnotationAtCoordinate(coordinate: CLLocationCoordinate2D) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = "An annotation!"
+        mapView.addAnnotation(annotation)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        mapView.delegate = self
+        
+        // set the region to display, this also sets a correct zoom level
+        // set starting center location in San Francisco
+        let centerLocation = CLLocation(latitude: 37.7833, longitude: -122.4167)
+        goToLocation(centerLocation)
+        addAnnotationAtCoordinate(centerLocation.coordinate)
+    }
+    
+    func goToLocation(location: CLLocation) {
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let region = MKCoordinateRegionMake(location.coordinate, span)
+        mapView.setRegion(region, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
